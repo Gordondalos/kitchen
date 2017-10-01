8139ecacd72842f7448b555e22f09aa571959086
Evgenii Bondarenko <gordondalos@gmail.com>
2017-10-01 22:21:47 +0600

    Реализовал вывод списка рецептов

---
+++ src/app/models/recipe.models.ts
@@ -0,0 +1,7 @@
+export interface Recipe {
+  id: string;
+  title: string;
+  ingUrl: string;
+  description: string;
+  ingredients: any;
+}
--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../service/recipe.service';
+import { Recipe } from '../models/recipe.models';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,11 +10,14 @@ import { RecipeService } from '../service/recipe.service';
 export class RecipeListComponent implements OnInit {
 
 
+  recipeList: Recipe[];
+
   constructor(
       private recipeService: RecipeService,
   ) { }
 
   ngOnInit() {
+    this.recipeList = this.recipeService.getRecipe();
   }
 
 
