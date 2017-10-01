917ad1565c4779d782ff193d708a2b92bf642d05
Evgenii Bondarenko <gordondalos@gmail.com>
2017-10-02 00:08:20 +0600

    Реализовал диалоги добавления и удаления ингридиентов

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecipeService } from '../service/recipe.service';
 import { Recipe } from '../models/recipe.models';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-recipe',
@@ -13,8 +14,8 @@ export class RecipeComponent implements OnInit {
   recipeId: string;
   recipe: Recipe;
   display: boolean = false;
-
-
+  title: string;
+  quantity: string;
 
   constructor(private route: ActivatedRoute,
               private recipeService: RecipeService,) {
@@ -37,6 +38,21 @@ export class RecipeComponent implements OnInit {
 
   showDialog() {
     this.display = true;
+  }
+
+  addIngridient() {
+    this.recipe.ingredients.push({ title: _.clone(this.title), quantity: _.clone(this.quantity) });
+    this.title = '';
+    this.quantity = '';
+    this.display = false;
+  }
+
+  removeIngridient(ingridientTitle) {
+    _.each(this.recipe.ingredients, (item, index) => {
+      if (item.title === ingridientTitle) {
+        this.recipe.ingredients.splice(index, 1);
+      }
+    })
   }
 
 
