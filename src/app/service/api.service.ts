5499193ce928596ea51fd07c05cf6fd7514c2a56
Evgenii Bondarenko <gordondalos@gmail.com>
2017-10-09 02:58:03 +0600

    delete trash

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -14,32 +14,9 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class ApiService {
 
-  apiUrl = 'http://localhost:8000';
 
   constructor(
-    private httpClient: HttpClient,
-    private http: Http) {
-  }
-
-  getUsers(): Promise <User[]> {
-    return new Promise ((resolve) => {
-      this.httpClient.get(this.apiUrl + '/user').subscribe((data) => {
-        resolve(data);
-      });
-    })
-  }
-
-// /user/{id}/edit
-  updateUser(user: User) {
-    const headers = new Headers ( {
-      'Content-Type' : 'application/json; charset=UTF-8',
-      'format' : 'json',
-      'URI' : '/user/' + user.id + '/edit'
-    } );
-    const options = new RequestOptions ({ headers : headers });
-    const data = JSON.stringify(user);
-    return this.http.put( this.apiUrl + '/user/' + user.id + '/edit', data, options )
-      .toPromise()
+   ) {
   }
 
 
